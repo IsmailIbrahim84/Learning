e0c29b24771667b441c5cd9e57dc826f5d7971b5
Ismael Ibrahim <beisso75@hotmail.com>
2019-01-19 20:06:18 +0200

    1

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 //Functions:
-function parametersAsFunction(func) {
-    func();
-}
+// function parametersAsFunction(func) {
+//     func();
+// }
 
-parametersAsFunction (hellowWorld);
+// parametersAsFunction (hellowWorld);
 
-function hellowWorld() {
-    alert("Hello, function!!!")
+// function hellowWorld() {
+//     alert("Hello, function!!!")
     
-}
+// }
+//-------------------------------------
+
+
+//Anonymous function:
+var isAdult = function (age) {
+    if (age >= 18) {
+        return "Yes";
+    }
+
+    else
+    {
+        return "No";
+    }
+};
+alert (isAdult(19));
